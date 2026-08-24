098291a5d825ad3b59c82187da6b4646010a4858
rachelmathilda <rachelmathilda10@gmail.com>
2026-08-24 08:33:23 +0700

    fix: kotlin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.example.vaelys"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
